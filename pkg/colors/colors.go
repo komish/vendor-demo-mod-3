edf88b57289d51bab788c2dc47c838aea5378c6e
Jose R. Gonzalez <josegonzalez89@gmail.com>
2020-10-06 09:35:52 -0500

    inital commit

---
+++ pkg/colors/colors.go
@@ -0,0 +1,8 @@
+// Package colors provides access to the Color type
+package colors
+
+// Color is the type for a color which contains english and spanish names for that color.
+type Color struct {
+	NameEnglish string
+	NameSpanish string
+}
